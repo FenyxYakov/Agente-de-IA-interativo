730a088cb0041650ee79cb6e6dc109d974aac132
Gustavo Silva <gugustavosilvac@gmail.com>
2026-03-05 19:42:40 -0300

    Update agente.py

--- agente.py
+++ agente.py
@@ -1,18 +1,18 @@
 import ollama
 
 def agente_interativo():
-    print("="*40)
+    print("="*60)
     print("AGENTE DE IA - (MISTRAL)")
     print("Instruções: Digite sua pergunta ou 'sair' para encerrar.")
-    print("="*40)
+    print("="*60)
 
     historico = [{'role': 'system', 'content': 'Você é um assistente. Responda de forma clara e objetiva em português.'}]
 
     while True:
-        pergunta = input("\n Você: ")
+        pergunta = input("Pergunta: ")
 
-        if pergunta.lower() in ['sair', 'exit', 'quit']:
-            print("IA: Até a proxima!.")
+        if pergunta.lower() in ("sair"):
+            print("Até a proxima!")
             break
 
         historico.append({'role': 'user', 'content': pergunta})
@@ -22,14 +22,12 @@ def agente_interativo():
             response = ollama.chat(model='mistral', messages=historico) 
             resposta_ia = response['message']['content']
             print(f"IA: {resposta_ia}")
+            print ("-"*40)
             historico.append({'role': 'assistant', 'content': resposta_ia})
         except Exception as e:
-            print(f"Erro ao conectar com o Ollama: {e}")
+            print(f"\nErro ao conectar com o Ollama: {e}")
             print("Certifique-se de que o Ollama está aberto e o modelo Mistral foi baixado.")
             break
 
 if __name__ == "__main__":
     agente_interativo()
-
-
-
